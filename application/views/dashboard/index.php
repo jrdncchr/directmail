27f8052ece97c962e1cfe65f5b9bc596e0efd852
Jordan Cachero <jrdncchr@gmail.com>
2017-03-06 21:48:43 +0800

    Added welcome message

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,7 +1,8 @@
 <div id="app">
     <div class="row">
         <div class="col-sm-12">
-            
+        <br />
+            Welcome back, <?php echo $logged_user->first_name . ' ' . $logged_user->last_name; ?>!
         </div>
     </div>
 </div>
